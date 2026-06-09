72329567a5f1be6c72675180eaf2e893eb9b83ce
Thimeo-dev <thimeosousa02@gmail.com>
2026-06-09 15:34:07 +0000

    ebook simple

--- books.js
+++ books.js
@@ -1,5 +1,5 @@
 // Liste locale des fichiers EPUB pour les appareils qui ne peuvent pas lister un dossier automatiquement.
-// Ajoutez ou supprimez ici les fichiers présents dans le dossier "dossier-livres".
+// Ajoutez ici les noms des fichiers présents dans le dossier "dossier-livres".
 window.livresManuels = [
     "pg2701-images-3.epub"
 ];
